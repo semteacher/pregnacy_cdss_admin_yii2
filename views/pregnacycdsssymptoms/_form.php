0805f33b365eac0669755c3577789e04845e7ac7
semteacher <semteacher@gmail.com>
2015-04-07 15:21:40 +0300

    change textfield to dropDownList in the form

--- views/pregnacycdsssymptoms/_form.php
+++ views/pregnacycdsssymptoms/_form.php
@@ -2,17 +2,23 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper; // load classes
+use app\models\PregnacyCdssSymptCategory;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\PregnacyCdssSymptoms */
 /* @var $form yii\widgets\ActiveForm */
+
+$dataList=ArrayHelper::map(PregnacyCdssSymptCategory::find()->asArray()->all(), 'id', 'cat_name');
 ?>
 
 <div class="pregnacy-cdss-symptoms-form">
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id_category')->textInput() ?>
+    <?//= $form->field($model, 'id_category')->textInput() ?>
+    <?=$form->field($model, 'id_category')->dropDownList($dataList, 
+         ['prompt'=>'--- Choose a Category: ---']) ?>
     
     <?= $form->field($model, 'symp_name')->textInput(['maxlength' => 50]) ?>
 
